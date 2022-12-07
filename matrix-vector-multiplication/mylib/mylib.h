f4776d90f9644ce7c8c8fd8708634adca025ca1f
IvanditoRakaputra <114067618+IvanditoRakaputra@users.noreply.github.com>
2022-12-07 20:35:18 +0700

    Add files via upload

---
+++ matrix-vector-multiplication/mylib/mylib.h
@@ -0,0 +1,6 @@
+void printMat(int* A, int N);
+void printVec(int* B, int N);
+int* matVecMult(int* M, int* v, int *result, int N);
+int generateRandomInt(int lower, int upper);
+void initMat(int* M, int N);
+void initVec(int* v, int N);
